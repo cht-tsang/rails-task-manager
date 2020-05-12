26f73b433e77803fd6419852dea85ca95d1a73cb
chi tsang <cht.chihungtsang@gmail.com>
2020-05-12 11:36:28 +0100

    rails new

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_102220) do
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.string "details"
-    t.boolean "completed"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
