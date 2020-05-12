7dd64976e277cbcb5ca7fd61c0cc200bd56930b8
chi tsang <cht.chihungtsang@gmail.com>
2020-05-12 11:44:09 +0100

    added completed to tasks with default boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_103847) do
+ActiveRecord::Schema.define(version: 2020_05_12_104224) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "details"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed"
   end
 
 end
